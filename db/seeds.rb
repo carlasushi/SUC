772f39130fd2e8d51b0a713eafb5c62578ff7e63
Oscar Lindberg <oscar.hewitt.lindberg@gmail.com>
2019-11-18 15:02:38 +0100

    some seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,32 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.destroy_all
+Home.destroy_all
+Room.destroy_all
+Chore.destroy_all
+
+
+puts "Create users"
+
+
+oscar = User.create!(email: "oscar.hewitt.lindberg@gmail.com", password: "123456")
+
+carla = User.create!(email: "learntoglow@gmail.com", password: "123456")
+
+puts "Creating homes"
+
+lewagon = Home.new(name: "Le Wagon", user: oscar)
+smag = Home.new(name: "Smag", user: carla)
+
+puts "create room"
+
+Room.create!(name: "Bathroom", home: lewagon)
+Room.create!(name: "Kitchen", home: lewagon)
+Room.create!(name: "Livingroom", home: lewagon)
+
+Room.create!(name: "Kitchen", home: smag)
+Room.create!(name: "Bathroom", home: smag)
+Room.create!(name: "Livingroom", home: smag)
+
